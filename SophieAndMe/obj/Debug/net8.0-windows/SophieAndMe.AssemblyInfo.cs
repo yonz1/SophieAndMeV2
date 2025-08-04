bce1e0a731c416d31dede9bf76e62e5e0d520006
yonz1 <bastienpelle223@gmail.com>
2025-08-04 17:25:52 +0200

    0408

--- SophieAndMe/obj/Debug/net8.0-windows/SophieAndMe.AssemblyInfo.cs
+++ SophieAndMe/obj/Debug/net8.0-windows/SophieAndMe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SophieAndMe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2467dd845effd847d897564443edced2eecf24c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18ceff07b9a17d0a73f358b29c5cfff6e532782")]
 [assembly: System.Reflection.AssemblyProductAttribute("SophieAndMe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SophieAndMe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
