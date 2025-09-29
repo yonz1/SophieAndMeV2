59eb3bfeba19bd4a1c25ebbc79bf278bb6c3596f
yonz1 <bastienpelle223@gmail.com>
2025-09-29 19:02:31 +0200

    nothing

--- SophieAndMe/obj/Release/net8.0-windows/SophieAndMe.AssemblyInfo.cs
+++ SophieAndMe/obj/Release/net8.0-windows/SophieAndMe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SophieAndMe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c5979a81eecd7b984679a165e285ed72a6c073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e75c78f37c98fa6c7e9e3d4da069c71d1695f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("SophieAndMe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SophieAndMe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
